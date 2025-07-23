c20085e172d79bb550b480500d468759a30d7957
cyrhades <cyrhades76@gmail.com>
2025-07-23 13:07:17 +0200

    modif pour proxy

--- .docker/server_http/www/src/Controller/ProfilController.php
+++ .docker/server_http/www/src/Controller/ProfilController.php
@@ -10,19 +10,11 @@ class ProfilController extends AbstractController
 {
     public function profil1() 
     {  
-        if (!isset($_SESSION['user'])) {
-            header('Location: login.php');
-            exit;
-        }
         return $this->render('profil1', ['user' => $_SESSION['user']]);
     }
 
     public function profil2() 
     {
-        if (!isset($_SESSION['user'])) {
-            header('Location: login.php');
-            exit;
-        }
         return $this->render('profil2', ['user' => $_SESSION['user']]);
     }
 }
